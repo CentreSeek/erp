48c1cb63e8c9928f18e270e85aeed36f38b15d6f
CentreS <centreseek@126.com>
2019-11-25 17:22:27 +0800

    修改文本错误

---
+++ src/main/java/com/yjjk/erp/ErpApplication.java
@@ -0,0 +1,16 @@
+package com.yjjk.erp;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(value = "com.yjjk.monitor.mapper")
+public class ErpApplication {
+
+
+	public static void main(String[] args) {
+    	SpringApplication.run(ErpApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/yjjk/erp/service/BaseService.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2019, 义金(杭州)健康科技有限公司
+ * FileName: BaseService
+ * Author:   CentreS
+ * Date:     2019-06-20 16:34
+ * Description: base service
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.yjjk.erp.service;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author CentreS
+ * @Description: base service
+ * @create 2019-06-20
+ */
+public class BaseService {
+    protected static Logger logger = LoggerFactory.getLogger(BaseService.class);
+
+
+}
