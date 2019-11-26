5eda2b538bb4042774f66875ed733d00c1011c29
CentreS <centreseek@126.com>
2019-11-26 20:47:27 +0800

    temp

--- src/main/java/com/yjjk/erp/controller/RateController.java
+++ src/main/java/com/yjjk/erp/controller/RateController.java
@@ -1,6 +1,18 @@
 package com.yjjk.erp.controller;
 
-import org.springframework.stereotype.Controller;
+import com.yjjk.erp.configer.CommonResult;
+import com.yjjk.erp.constant.ErrorCodeEnum;
+import com.yjjk.erp.entity.vo.RatesInfoVO;
+import com.yjjk.erp.utility.ResultUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @program: YjjkErp
@@ -8,6 +20,20 @@ import org.springframework.stereotype.Controller;
  * @author: CentreS
  * @create: 2019-11-25 19:24:41
  **/
-@Controller
+@RestController
+@Api(tags = "备案进度管理模块")
+@RequestMapping("rate")
 public class RateController extends BaseController {
+
+    @ApiOperation("备案详情记录")
+    @RequestMapping(value = "/rates", method = RequestMethod.GET)
+    public CommonResult<List<RatesInfoVO>> getHospitalInfo(@ApiParam(value = "医院id") @RequestParam("hospitalId") Integer hospitalId) {
+        try {
+            List<RatesInfoVO> rateInfoList = super.rateService.getRateInfoList(hospitalId);
+            return ResultUtil.returnSuccess(rateInfoList);
+        } catch (Exception e) {
+            logger.error("业务异常信息：[{}]", e.getMessage(), e);
+        }
+        return ResultUtil.returnError(ErrorCodeEnum.UNKNOWN_ERROR);
+    }
 }
